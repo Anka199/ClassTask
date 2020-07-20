4cd1fb843d66923db7229038166830f4e01b0dcc
Anka199 <61878254+Anka199@users.noreply.github.com>
2020-07-20 10:41:15 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,17 +1,21 @@
 let matematika = 80
-let bahasaIndo = 89
+let bahasaIndo = 90
 let bahasaInggris = 89
 let ipa = 69
 let average = (matematika + bahasaIndo + bahasaInggris + ipa) / 4
 
-if (average >= 90) {
-    console.log("Rata-rata :" + average + ". Grade : A");
-} else if (average >= 80) {
-    console.log("Rata-rata :" + average + ". Grade : B");
-} else if (average >= 70) {
-    console.log("Rata-rata :" + average + ". Grade : C");
-} else if (average >= 60) {
-    console.log("Rata-rata :" + average + ". Grade : D");
+if (matematika == 'number', bahasaIndo == 'number', bahasaInggris == 'number', ipa == 'number') {
+    if (average >= 90) {
+        console.log("Rata-rata :" + average + ". Grade : A");
+    } else if (average >= 80) {
+        console.log("Rata-rata :" + average + ". Grade : B");
+    } else if (average >= 70) {
+        console.log("Rata-rata :" + average + ". Grade : C");
+    } else if (average >= 60) {
+        console.log("Rata-rata :" + average + ". Grade : D");
+    } else {
+        console.log("Rata-rata :" + average + ". Grade : E");
+    }
 } else {
-    console.log("Rata-rata :" + average + ". Grade : E");
+    console.log("Data Tidak Valid")
 }
