d687c9a4df9412d014cfdb8f6b1cfb60641bd7a3
Anka199 <61878254+Anka199@users.noreply.github.com>
2020-07-20 10:39:52 +0700

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,18 +1,20 @@
 let data = {
     id: 1,
-    name: "Anka Abdullah",
+    name: "Leanne Graham",
     username: "BossDarling",
-    email: "ankabdullah.bogor@gmail.com",
+    email: "Sincere@april.biz",
     addres: {
         street: "Kulas Light",
         suite: "Apt. 556",
         city: "Gwenbourgh",
         zipcode: "92998-3874",
     },
-    hobby: "Winninng the Batle",
     phone: "1-770-736-8031 x56442",
     website: "hildegard.org",
 }
+
+let newData = {...data, name: "Anka Abdullah", hobby: "Running", email: "ankabdullah.bogor@gmail.com" }
+console.log(newData)
 
 const { street, city } = data.addres
 console.log(street)
